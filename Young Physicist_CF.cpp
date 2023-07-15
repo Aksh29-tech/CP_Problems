ca709a41654a71252decdf3dd72db0dd1b48c210
Akshat Nigam <nigamakshat29jan2000@gmail.com>
2023-07-15 12:12:36 +0530

    Create Young Physicist_CF.cpp

---
+++ Young Physicist_CF.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+   int n;
+   cin>>n;
+   int a[n][3];
+   for(int i=0;i<n;i++){
+       for(int j=0;j<3;j++){
+           cin>>a[i][j];
+       }
+   }
+   int sum1=0,sum2=0,sum3=0;
+   for(int i=0;i<n;i++){
+       sum1+=a[i][0];
+       sum2+=a[i][1];
+       sum3+=a[i][2];
+   }
+   if(sum1==0 && sum2==0 && sum3==0)
+      cout<<"YES"<<endl;
+   else
+      cout<<"NO"<<endl;
+}
